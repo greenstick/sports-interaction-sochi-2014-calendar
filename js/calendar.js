d7dbbbe4c879bfdcd8d0b22396e91a0dc8b5c7f2
Greenstick <benacordier@gmail.com>
2013-11-25 15:23:50 -0800

    Update to calendar class, now accepts strokeWidth and strokeColor params

--- js/calendar.js
+++ js/calendar.js
@@ -3,7 +3,7 @@
 *	Calendar Class
 **/
 
-Calendar = function (r1, r2, margins, colors, days, sports)
+Calendar = function (r1, r2, margins, strokeWidth, strokeColor, colors, days, sports)
 {
 //Defining variables
 	var pi = Math.PI;
@@ -19,6 +19,8 @@ Calendar = function (r1, r2, margins, colors, days, sports)
 	chart.margins = margins;
 	chart.height = (chart.outer * 2) + chart.margins.top + chart.margins.bottom;
 	chart.width = (chart.outer * 2) + chart.margins.left + chart.margins.right;
+	chart.strokeWidth = strokeWidth;
+	chart.strokeColor = strokeColor;
 	chart.colors = colors;
 	chart.rings = days;
 	chart.segments = sports;
@@ -42,7 +44,7 @@ Calendar = function (r1, r2, margins, colors, days, sports)
 		.attr("height", chart.height);
 
 //Generating sunburst through params & appending data
-	chart.sunburst = function (outer, inner, rings, segments, colors) {
+	chart.sunburst = function (outer, inner, strokeColor, strokeWidth, rings, segments, colors) {
 		var ringWidth = (outer - inner)/rings;
 		for(var i = 0; i < days; i++) {
 			for(var j = 0; j < segments; j++) {
@@ -54,14 +56,14 @@ Calendar = function (r1, r2, margins, colors, days, sports)
 						svg.append("path")
 							.attr("d", arc)
 							.attr("fill", chart.colors[j])
-							.attr("stroke", "#000E5C")
-							.attr("stroke-width", "2px")
+							.attr("stroke", strokeColor)
+							.attr("stroke-width", chart.strokeWidth + "px")
 							.attr("transform", "translate(" + chart.width/2 + ", " + chart.height/2 + ")");
 			};
 		};
 	};
 //Drawing sunburst
-	chart.sunburst(chart.outer, chart.inner, chart.rings, chart.segments, chart.colors);
+	chart.sunburst(chart.outer, chart.inner, chart.strokeColor, chart.strokeWidth, chart.rings, chart.segments, chart.colors);
 };
 
-var calendar = new Calendar(300, 50, {top: 40, right: 40, bottom: 40, left: 40}, ['#1E0B7F', '#231355', '#2E1562', '#410D65', '#550D38', '#533136', '#544C35', '#52563A', '#445A38', '#285833', '#1C4633', '#112938', '#1C4C55', '#1E5070', '#1B3C72'], 16, 15);
+var calendar = new Calendar(400, 50, {top: 40, right: 40, bottom: 40, left: 40}, 2, '#0E0E22', ['#1E0B7F', '#231355', '#2E1562', '#410D65', '#550D38', '#533136', '#544C35', '#52563A', '#445A38', '#285833', '#1C4633', '#112938', '#1C4C55', '#1E5070', '#1B3C72'], 30, 15);
